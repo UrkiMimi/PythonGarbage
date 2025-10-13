5e63c07098c16bab7a83440c1f480f1cdc8586f9
Urki <74517821+UrkiMimi@users.noreply.github.com>
2025-10-13 03:11:54 -0500

    add more arguments

--- YTRandomizer/README.md
+++ YTRandomizer/README.md
@@ -1,7 +1,13 @@
 # YoutubeRandomizer
 A hacked together script that lets you get random videos from somebody's channel! Channel IDs are only supported.
 
-### Usage
-```
-python randomizer.py [channel ID] --regenerate [Regenerates playlist cache (debug)]`
+### Arguments
+- Channel ID (First argument): The YouTube channel ID you're going to grab videos from. Must be the first argument
+- API `--api`: The YouTube V3 API key. Alternatively, you can make put the API key in a file called `API`
+- Regenerate `--regenerate`: Regenerates the playlist cache.
+- API timeout `--timeout`: Wait time in seconds before next request if needed. Defaults to one second.
+
+**Example**
+```bash
+python randomizer.py UCoFUO4AixQfs-_oXbmbMqag --regenerate --api [key] --timeout 4
 ```
--- YTRandomizer/randomizer.py
+++ YTRandomizer/randomizer.py
@@ -6,7 +6,7 @@ import random as rand
 #region init
 ### get channel from args and exit if they are empt
 if len(sys.argv[1:]) == 0:
-    print('Usage: randomizer.py [youtube channel id]')
+    print('Usage: randomizer.py [channel ID] --regenerate [Regenerates playlist cache (debug)]` --api [API key] --timeout [number]')
     exit()
 
 # channel arguments
@@ -14,15 +14,52 @@ channel = sys.argv[1]
 videos = []
 seed = int.from_bytes(os.urandom(4))
 chunk = 1
+api = ''
+sleepTime = 1
 
-# get api key
-with open('API', 'r') as f:
-    api = f.read()
+# find other arguments
+for arg in range(len(sys.argv[1:])):
+    # regenerate
+    if (sys.argv[arg+1] == '--regenerate'):
+        try:
+            os.remove('playlistCache.json')
+        except:
+            print(colorama.Fore.BLUE + 'Cache not found. Nothing to do.' + colorama.Fore.WHITE)
+
+    # api key
+    if (sys.argv[arg+1] == '--api'):
+        try:
+            api = sys.argv[arg + 2]
+        except:
+            print(colorama.Fore.RED + 'Error: You must put in an API key!' + colorama.Fore.RED)
+            sys.exit()
+    
+    # timeout
+    if (sys.argv[arg+1] == '--timeout'):
+        try:
+            # non negative duct tape fix lmao
+            if (sleepTime < 0):
+                print(colorama.Fore.RED + 'Error: timeout time must be non-negative!' + colorama.Fore.WHITE)
+                sys.exit()
+            else:
+                sleepTime = float(sys.argv[arg + 2])
+        except:
+            sleepTime = 1
+
+
+# get api key if not specified
+if api == '':
+    try:
+        with open('API', 'r') as f:
+            api = f.read()
+    except:
+        print(colorama.Fore.RED + 'Error: API key not found. Please specify a key using "--api" or create a file named API with the key inside.' + colorama.Fore.WHITE)
+        sys.exit()
 
 
 # check if channel is in cache
 try:
-    with open('playlistCache', 'r') as f:
+    with open('playlistCache.json', 'r') as f:
         tmp = json.loads(f.read())
 
         newChannel = True
@@ -43,8 +80,10 @@ except:
 if newChannel:
     request = requests.get(f'https://www.googleapis.com/youtube/v3/channels?part=statistics,contentDetails&id={channel}&key={api}')
     # check if request is valid
+    if (request.status_code == 403):
+        print(colorama.Fore.RED + 'Error: Response was forbidden! Did you put in a correct API key?' + colorama.Fore.WHITE)
     if (request.status_code != 200):
-        print(f'Response failed! Returned with HTTP code {request.status_code}.')
+        print(colorama.Fore.red + f'Response failed! Returned with HTTP code {request.status_code}.' + colorama.Fore.WHITE)
         sys.exit()
 
     # set total videos and uploadid
@@ -75,8 +114,8 @@ if newChannel:
     # hammer api
     while (nextKey != ''):
         # delay to avoid ratelimiting
-        print(f'Waiting a second before next request. / Chunk: {chunk}/{round(totalVideos/50)}')
-        sleep(1)
+        print(colorama.Fore.BLUE + f'Waiting before next request. / Chunk: {chunk}/{round(totalVideos/50)}' + colorama.Fore.WHITE)
+        sleep(sleepTime)
         chunk+=1
         request = requests.get(f'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId={uploadsID}&maxResults=50&key={api}&pageToken={nextKey}')
 
@@ -99,7 +138,7 @@ if newChannel:
     playlistResult['videos'] = videos
     playlistResult['chID'] = channel
 
-    with open('playlistCache', 'w') as f:
+    with open('playlistCache.json', 'w') as f:
         f.write(json.dumps(playlistResult, indent=2))
 
 
@@ -108,9 +147,8 @@ if newChannel:
 rand.seed = seed
 pickedVideo = rand.choice(videos)
 pickedVideo = pickedVideo['contentDetails']['videoId']
-print(pickedVideo)
 
-print('Opening browser for selected video')
+print(f'Opening browser with video ID {pickedVideo}.')
 webbrowser.open(f'https://www.youtube.com/watch?v={pickedVideo}')
 
-print(f'Debug args, Video ID: {pickedVideo}, Channel ID: {channel}, Random state: {seed}, Total videos: {len(videos)}')
+#print(f'Debug args, Video ID: {pickedVideo}, Channel ID: {channel}, Random state: {seed}, Total videos: {len(videos)}')
