d8a222d87c64a9e4b007de63a6744842988dcbc0
Urki <74517821+UrkiMimi@users.noreply.github.com>
2025-10-23 13:00:33 -0500

    uimg

---
+++ uImg/read.py
@@ -0,0 +1,64 @@
+# libs
+import pygame, sys, zlib
+
+# open image 
+with open('smoker.uim', 'rb') as f:
+    uim = list(f.read())
+
+# get width and height
+index = 5
+width = ''
+height = ''
+
+# width loop
+while (uim[index] != 44):
+    width += chr(uim[index])
+    index+=1
+
+index += 1 
+
+# height loop
+while (uim[index] != 78):
+    height += chr(uim[index])
+    index+=1
+
+# convert to int
+width = int(width)
+height = int(height)
+
+
+# decompress data and read image
+dat = uim[16:]
+dat = bytes(dat)
+dat = zlib.decompress(dat)
+dat = list(dat)
+
+
+# initalize pygame
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+
+def drawUIM():
+    index = 0
+
+    # image loop
+    for y in range(height):
+        for x in range(width):
+            # determine color
+            color = (dat[index], dat[index+1], dat[index+2])
+            
+            index+=3
+
+            # render pixel
+            pygame.draw.rect(screen, color, (x,y,1,1))
+
+# draw image
+drawUIM()
+pygame.display.flip()
+
+# persistence
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
---
+++ uImg/uImg.py
@@ -0,0 +1,64 @@
+import math
+from PIL import Image
+import zlib
+
+# file format
+def uImg(width, height, data):
+    ### header and metadata
+    tmp = [0] * 16
+
+    # add header to first four bytes
+    headerTex = 'uImg'
+    for i in range(len(headerTex)):
+        tmp[i] = ord(headerTex[i])
+
+    
+    ### add width and height
+    # width
+    index = 5
+    for i in range(len(str(width))):
+        tmp[index] = ord(str(width)[i])
+        index+=1
+
+    # seperator then height
+    tmp[index] = ord(',')
+    index+=1
+    
+    for i in range(len(str(height))):
+        tmp[index] = ord(str(height)[i])
+        index+=1
+
+    # end byte
+    tmp[index] = ord('N')
+    
+    # image data
+    tmp += zlib.compress(bytes(data), level=9)
+
+    return bytes(tmp)
+
+#returns 8bit array of each channel
+def imageToArray(imagePath):
+    # Import and scale image
+    image = Image.open(imagePath).convert('RGB')
+    width, height = [round(image.size[0]), round(image.size[1])]
+    image = image.resize((width, height))
+
+    # Seperate image by channel
+    pixelR = image.getchannel('R').getdata()
+    pixelG = image.getchannel('G').getdata()
+    pixelB = image.getchannel('B').getdata()
+
+    tmp = []
+
+    # add to array
+    for r in range(width*height):
+        tmp.append(pixelR[r])
+        tmp.append(pixelG[r])
+        tmp.append(pixelB[r])
+
+    return tmp
+
+
+with open('smoker.uim' , 'wb') as f:
+   imgArray = imageToArray('smoker.jpg')
+   f.write(uImg(399,399,imgArray))
